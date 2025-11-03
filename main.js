aeb4e47ea7171e877650a3822bd35d0caeb4bc20
James McDermott <jamesmichaelmcdermott@gmail.com>
2025-11-03 16:49:34 +0000

    change to superformula3d for default

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const framework = new InteractiveEAFramework(SheepIndividual);
+const framework = new InteractiveEAFramework(SuperFormula3DIndividual);
 
 // Make framework globally accessible for 3D individuals
 window.framework = framework;
